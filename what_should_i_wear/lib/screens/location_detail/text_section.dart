f7c6ce5459a115580fe4be27b5d37d6b380e0227
Jules <Baldinijc@gmail.com>
2022-04-25 09:29:17 -0400

    updates and first screen

---
+++ what_should_i_wear/lib/screens/location_detail/text_section.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+  const TextSection({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.red,
+      ),
+      child: const Text(''),
+    );
+  }
+}
