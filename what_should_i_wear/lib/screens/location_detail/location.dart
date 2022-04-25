41ae97f091018e1fd5ae1d65d08c55b580e9335a
Jules <Baldinijc@gmail.com>
2022-04-25 09:28:44 -0400

    First screentest

---
+++ what_should_i_wear/lib/screens/location_detail/location.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:what_should_i_wear/screens/location_detail/text_section.dart';
+import 'text_section.dart';
+
+class LocationDetail extends StatelessWidget {
+  const LocationDetail({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextSection(),
+            TextSection(),
+            TextSection(),
+          ],
+        ));
+  }
+}
